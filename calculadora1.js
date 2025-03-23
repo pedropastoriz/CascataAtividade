671cc03b4738e609cbff99657ecc05351aedbdd2
Karine Ferreira Mendes <karineferreiramendes@gmail.com>
2025-03-23 14:21:46 -0300

    Att da calculadora1.js (sem express), tela da calculadora pronta + css atualizado

--- calculadora1.js
+++ calculadora1.js
@@ -1,27 +1,28 @@
-function calcular() {
+function calcular(operador) {
   const n1 = parseFloat(document.getElementById('n1').value);
   const n2 = parseFloat(document.getElementById('n2').value);
-  const operador = document.getElementById('operador').value;
-
   let resultado;
 
-  if (operador === '+') {
-      resultado = n1 + n2;
-  } else if (operador === '-') {
-      resultado = n1 - n2;
-  } else if (operador === '*') {
-      resultado = n1 * n2;
-  } else if (operador === '/') {
-      if (n2 === 0) {
-          alert("Divisor não pode ser igual a zero.");
-          return;
-      } else {
-          resultado = n1 / n2;
-      }
-  } else {
-      alert('Operação inválida.');
-      return;
+  // Verifica se os números são válidos
+  if (isNaN(n1) || isNaN(n2)) {
+    alert("Por favor, insira números válidos.");
+    return;
+}
+
+// Identifica o operador
+
+  switch(operador) {
+    case '+': resultado = n1 + n2; break;
+    case '-': resultado = n1 - n2; break;
+    case '*': resultado = n1 * n2; break;
+    case '/': 
+        if (n2 === 0) {
+        alert("Divisor não pode ser igual a zero.");
+        return;
   }
+    resultado = n1 / n2;
+    break;
+}
 
   // Arredondamento para 2 casas decimais
   resultado = Math.round(resultado * 100) / 100;
